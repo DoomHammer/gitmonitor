42cdd6ed220d944ea4c22124a8955ccd15e93dcf
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-01-04 17:56:12 +0100

    Allow multiple repos

--- main.py
+++ main.py
@@ -1,49 +1,70 @@
 from functools import partial
 
 from git import Repo
+from git.exc import InvalidGitRepositoryError
 
+repolist = [
+    "/tmp/test",
+    "/home/allgreed/Work/buxfer_connector",
+    "/home/allgreed/Work/doug",
+    "/tmp/ble",
+]
 
-def main():
-    repo = Repo(path="/tmp/test")
+def main(print_function=print):
+    for repo_path in repolist:
+        padded_print_function = partial(print_function, "\t")
 
-    for branch in repo.branches:
-        branch_information = get_branch_information(branch)
-        branch_information.output_messages()
+        try:
+            repo = Repo(path=repo_path)
+        except InvalidGitRepositoryError as e:
+            print_function("Path {} is not a valid git repo".format(e))
+        else:
+            branch_reports = [get_branch_information(branch) for branch in repo.branches]
+
+            if branch_reports:
+                print_function("In repo {}:".format(repo_path))
+                for branch_report in branch_reports:
+                    for message in branch_report.messages:
+                        padded_print_function(message)
 
 
 class BranchReport:
     def __init__(self):
-       self.messages = [] 
+       self._messages = [] 
+
+    @property
+    def messages(self):
+        return self._messages
 
     def add_message(self, message):
         self.messages.append(message)
 
-    def output_messages(self, fn=print):
-        for message in self.messages:
-            fn(message)
-
-
 def get_branch_information(branch):
-    current_branch_message = partial(branch_message, branch.name)
-    tracking_branch = branch.tracking_branch()
-    git_cmd = branch.repo.git
     report = BranchReport()
+
+    current_branch_message = partial(branch_message, branch.name)
+    git_cmd = branch.repo.git
+    tracking_branch = branch.tracking_branch()
 
     if not tracking_branch:
         report.add_message(current_branch_message("not tracked"))
-    else:
-        branches_to_compare = "{local}...{tracking}".format(local=branch.name, tracking=tracking_branch.name)
-        ahead, behind = intify(git_cmd.rev_list(branches_to_compare, count=True, left_right=True).split("\t"))
-        assert ahead >= 0
-        assert behind >= 0
+        return report
 
-        if ahead:
-            report.add_message(current_branch_message("ahead of tracking branch by {} commit{}".format(ahead, "s" if ahead > 1 else "")))
-        if behind:
-            report.add_message(current_branch_message("behind of tracking branch by {} commit{}".format(behind, "s" if ahead > 1 else "")))
+    if tracking_branch not in branch.repo.refs:
+        report.add_message(current_branch_message("bleble"))
+        return report
+
+    branches_to_compare = "{local}...{tracking}".format(local=branch.name, tracking=tracking_branch.name)
+    ahead, behind = intify(git_cmd.rev_list(branches_to_compare, count=True, left_right=True).split("\t"))
+    assert ahead >= 0
+    assert behind >= 0
+
+    if ahead:
+        report.add_message(current_branch_message("ahead of tracking branch by {} commit{}".format(ahead, "s" if ahead > 1 else "")))
+    if behind:
+        report.add_message(current_branch_message("behind of tracking branch by {} commit{}".format(behind, "s" if ahead > 1 else "")))
 
     return report
-
 
 def branch_message(branch_name=None, message=None):
     return "Branch {} is {}".format(branch_name, message)
